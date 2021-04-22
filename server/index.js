956e940bcb5ef7a856d2114f28ba0519e74bbdeb
MNK-FTHR <43670661+MNK-FTHR@users.noreply.github.com>
2021-04-23 00:05:00 +0200

    install front dependencies

--- server/index.js
+++ server/index.js
@@ -14,4 +14,4 @@ app.get('/', (req, res) => res.send('Hello World!'));
 require("./app/routes/client.routes.js")(app);
 // listen
 const port = config.app.PORT;
-app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`))
